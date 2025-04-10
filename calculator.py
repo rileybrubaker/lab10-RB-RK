da92ba1801469505aa3f3510eed04b35716c15b1
rkofman10 <rkofman@ufl.edu>
2025-04-10 14:18:42 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 #https://github.com/rileybrubaker/lab10-RB-RK.git
 #partner 1: Riley Brubaker
-#partner 2: Rachel Kofmanh
+#partner 2: Rachel Kofman
 """
 calculator.py
 - Defines functions used to create a simple calculator
