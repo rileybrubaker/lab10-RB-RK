7a1050b52bb82db4859fe708c4d32bb0eee3fbd7
rkofman10 <rkofman@ufl.edu>
2025-04-13 11:54:19 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,6 +9,33 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a +b
+
+def subtract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    if b==0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a/b
+
+def logarithm(value, base)"
+    if value<=0:
+        raise ValueError("Logarithm underfined for non-positive values")
+    if base<=0 or base==1:
+        raise ValueError("Logarithm base must be positive and not equal to 1")
+    return math.log(value, base)
+
+def hypotenuse(a, b):
+    return math.sqrt(a**2 +b**2)
+
+def square_root(x):
+    if x<0:
+        raise ValueError("Cannot take square root of a negative number")
+    return math.sqrt(x)
+
 
 
