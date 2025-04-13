6d5929bec85458fdb126f383210426c08a26cbc6
rkofman10 <rkofman@ufl.edu>
2025-04-13 11:57:43 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,10 +14,10 @@ def add(a, b):
 def subtract(a, b):
     return a-b
 
-def multiply(a, b):
+def mul(a, b):
     return a*b
 
-def divide(a, b):
+def div(a, b):
     if b==0:
         raise ZeroDivisionError("Cannot divide by zero")
     return a/b
